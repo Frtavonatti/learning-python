f3bdca4e2779ba95d4f739f8284e882a0b4cae0c
frtavonatti <frtavonatti@gmail.com>
2026-01-25 17:11:59 +0000

    Part2: added basic movement to the ship

---
+++ Part2_Pygame/game_functions.py
@@ -0,0 +1,30 @@
+import sys
+import pygame
+
+
+def check_events(ship):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_q
+        ):
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
+
+
+def update_screen(ai_settings, screen, ship):
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently drawn screen visible
+    pygame.display.flip()
