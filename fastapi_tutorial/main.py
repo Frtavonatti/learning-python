68228abf270f9d09b0f5720286a0f79c42ea776f
frtavonatti <frtavonatti@gmail.com>
2026-02-07 22:14:47 +0000

    Django: created topic and entry models

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
 
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 app = FastAPI()
 
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW"}
+    elif model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
