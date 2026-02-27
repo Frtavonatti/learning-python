bb9a1a8a3ffbc069bc830a36278707bb8ecd4ff0
frtavonatti <frtavonatti@gmail.com>
2026-02-27 11:30:18 +0000

    Django: protected user_topic and edit_entry views

--- crash_course/django/learning_logs/views.py
+++ crash_course/django/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import (
     reverse,
 )  # Django >= 2.0 ## from django.core.urlresolvers import reverse  # Django < 2.0 (legacy)
@@ -15,7 +15,7 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
 
@@ -23,6 +23,9 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        return Http404
+
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -35,7 +38,9 @@ def new_topic(request):
     else:
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse("learning_logs:topics"))
 
     context = {"form": form}
@@ -64,6 +69,8 @@ def new_entry(request, topic_id):
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        return Http404
 
     if request.method != "POST":
         form = EntryForm(instance=entry)
